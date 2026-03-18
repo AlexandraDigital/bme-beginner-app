9d8de4cb71625ff87a6b3d213a47b67a2de88cf6
AlexandraDigital <futuresuccess105@gmail.com>
2026-03-18 15:57:34 -0400

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "YOUR_SUPABASE_URL",
-  "YOUR_SUPABASE_ANON_KEY"
+  "https://rgauauhykivwollavzjp.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJnYXVhdWh5a2l2d29sbGF2empwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4NjA3ODgsImV4cCI6MjA4OTQzNjc4OH0.sJuaeTmYbZcEtwlmfosgiWJnfo8jtNq8BPCCjjF8Y7I"
 );
